177dfbf7abc0c3ce386232347ffdc3e5a909b47d
Bengi Anıl <bengianil34@gmail.com>
2023-03-10 13:43:56 +0300

    Initial Commit

---
+++ NewProject/NewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectApp.swift
+//  NewProject
+//
+//  Created by Bengi Anıl on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
